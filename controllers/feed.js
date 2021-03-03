915ac3092a799a8a5ca2749f0a35441707f09822
Ricardo <rdobmk@gmail.com>
2021-03-03 21:43:03 +0100

    api validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,22 +1,41 @@
+const { validationResult, body } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
+            _id: '1',
             title: 'First Post',
-            content: 'This is the first content'
+            content: 'This is the first content',
+            imageUrl: 'images/testt.jpg',
+            creator: {
+                name: 'Richard'
+            },
+            createdAt: new Date()
         }]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json(
+            {
+                message: 'Validation failed, entered data is incorrect',
+                errors: errors.array()
+            }
+        )
+    }
     const title = req.body.title;
     const content = req.body.content;
     // Create post in db
     res.status(201).json({
         message: 'Post created Succesfully',
         post: {
-            id: new Date().toISOString(),
+            _id: new Date().toISOString(),
             title: title,
-            content: content
+            content: content,
+            creator: { name: 'Richard'},
+            createdAt: new Date()
         }
     });
 };
