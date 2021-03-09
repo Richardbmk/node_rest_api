8b8b5f2eddfda0b51df356761dbb946df93ded63
Ricardo <rdobmk@gmail.com>
2021-03-09 21:20:45 +0100

    end of restApi basic

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
+// MongoDB Atlas configuration
+require('dotenv').config();
+const db_user = process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
+const db_name = process.env.DB_NAME;
+const db_url_cloud = `mongodb+srv://${db_user}:${db_password}@cluster0-a0s4s.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 app.use(bodyParser.json()); // application/json
 
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
@@ -16,5 +51,24 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8080);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+    .connect(db_url_cloud)
+    .then(() => {
+        app.listen(8080, () => {
+            console.log("The REST API is running on port 8080");
+        });
+    })
+    .catch(err => {
+        console.log(err);
+});
--- routes/feed.js
+++ routes/feed.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const { body } = require('express-validator/check');
+const isAuth = require('../middleware/is-auth');
 
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post('/post',
+router.post('/post', isAuth,
      [
          body('title')
             .trim()
-            .isLength({ min: 7 }),
+            .isLength({ min: 5 }),
         body('content')
             .trim()
             .isLength({ min: 5 })
-     ], feedController.createPost);
+     ], feedController.createPost
+);
 
+
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+// PUT /post/:postId
+router.put('/post/:postId', isAuth,
+     [
+         body('title')
+            .trim()
+            .isLength({ min: 5 }),
+        body('content')
+            .trim()
+            .isLength({ min: 5 })
+     ], feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
